f0a637b9de722a24d3af35add35db11e8efd21a0
Badrutdinovii <izater@mail.ru>
2020-10-22 23:37:22 +0300

    1/3 HTTP examle from metanit.com/web/angular2/6.1.php

--- purchaseapp/src/app/app.module.ts
+++ purchaseapp/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+
 @NgModule({
-    imports:      [ BrowserModule, FormsModule ],
+    imports:      [ BrowserModule, FormsModule, HttpClientModule ],
     declarations: [ AppComponent ],
     bootstrap:    [ AppComponent ]
 })
