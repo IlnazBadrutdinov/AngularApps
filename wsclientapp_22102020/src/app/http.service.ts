5ef71219b9e131d55deae072964fb30080f092ee
Badrutdinovii <izater@mail.ru>
2020-11-15 23:44:25 +0300

    wsclientapp - пробую отправить запрос на свой сервис
    - заменил ссылку
    - заменил входной запрос на явный

--- wsclientapp_22102020/src/app/http.service.ts
+++ wsclientapp_22102020/src/app/http.service.ts
@@ -10,6 +10,8 @@ export class HttpService {
   postData(user: User) {
     console.log(user);
     console.log('BadrutdinovII');
-    return this.http.post('http://localhost:3000/postuser', user);
+    let xmlString = "<requestData><integroMethod>123</integroMethod></requestData>";
+    console.log(xmlString);
+    return this.http.post(/*'http://localhost:3000/postuser'*/'http://localhost:8080/oct05_2020_1_1_0_SNAPSHOT_war/hello', xmlString);
   }
 }
